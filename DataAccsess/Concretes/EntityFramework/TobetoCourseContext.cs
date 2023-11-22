8aea966c91f98f8dec81dc72068d042be998ee2d
orhanemreonal <orhanemreonal@gmail.com>
2023-11-22 12:11:11 +0300

    Add project files.

---
+++ DataAccsess/Concretes/EntityFramework/TobetoCourseContext.cs
@@ -0,0 +1,27 @@
+﻿using Entities.Concretes;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concretes.EntityFramework
+{
+    public class TobetoCourseContext:DbContext
+    {
+      
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=TobetoCourseAcademy;Trusted_Connection = true");
+
+            //optionsBuilder.UseNpgsql("Server=localhost;Port=5432;Database=TobetoCourseAcademy;User ID=postgres;Password=12345;Integrated Security=true;Pooling=true;");
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Instructor> Instructors { get; set; }
+
+    }
+}
---
+++ Entities/Concretes/Course.cs
@@ -0,0 +1,24 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Entities.Concretes
+{
+    public class Course : IEntity
+    {
+
+        public int CourseId { get; set; }
+        public int CategoryId { get; set; }
+        public int InstructorId { get; set; }
+        public string CourseName { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public string ImageUrl { get; set; }
+        public Category? Category { get; set; }
+        public Instructor? Instructor { get; set; }
+    }
+}
